cd7377cada8ca88f1f75595cfccf44bee4bb20a7
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2023-08-02 08:51:47 -0300

    version actulizada en la nube

--- HolaPepe/Form1.Designer.cs
+++ HolaPepe/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(103, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hola Pepe";
+            this.label1.Text = "Hola Pepe que tal";
             // 
             // Form1
             // 
